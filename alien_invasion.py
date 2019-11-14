e0637a5b1629c8e00556a23a7c9344cb0b9ab6c7
ycbw <bwyuchi@163.com>
2019-11-14 11:58:57 +0800

    game structure

---
+++ alien_invasion.py
@@ -0,0 +1,19 @@
+import sys
+import pygame
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    run_game()
