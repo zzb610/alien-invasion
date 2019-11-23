a0d3c5959fe39255d8b4e77984dea8cfd4f0d2bf
ycbw <bwyuchi@163.com>
2019-11-23 21:06:33 +0800

    change bullet width

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings():
 
         # bullet settings
         self.bullet_speed_factor = 3
-        self.bullet_width = 300
+        self.bullet_width = 10
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_allowed = 5
